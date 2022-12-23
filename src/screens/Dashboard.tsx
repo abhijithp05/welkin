707eaba67d986f70ad222f8bb267f5468b841fe7
abhijithp05 <abhijithp05@gmail.com>
2022-12-23 18:56:58 +0530

    Implement export csv

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -12,6 +12,61 @@ export const Dashboard: React.FC = (): JSX.Element => {
   const [selectedTask, setSelectedTask] = useState<number[]>([]);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
+  function tableToCSV() {
+    // Variable to store the final csv data
+    var csv_data: any = [];
+
+    // Get each row data
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+      // Get each column data
+      var cols = rows[i].querySelectorAll("td,th");
+
+      // Stores each csv row data
+      var csvrow = [];
+      for (var j = 0; j < cols.length; j++) {
+        // Get the text data of each cell
+        // of a row and push it to csvrow
+        csvrow.push(cols[j].innerHTML);
+      }
+
+      // Combine each column value with comma
+      csv_data.push(csvrow.join(","));
+    }
+
+    // Combine each row data with new line character
+    csv_data = csv_data.join("\n");
+
+    // Call this function to download csv file
+    downloadCSVFile(csv_data);
+  }
+
+  function downloadCSVFile(csv_data: any) {
+    // Create CSV file object and feed
+    // our csv_data into it
+    let CSVFile = new Blob([csv_data], {
+      type: "text/csv",
+    });
+
+    // Create to temporary link to initiate
+    // download process
+    var temp_link = document.createElement("a");
+
+    // Download csv file
+    temp_link.download = "GfG.csv";
+    var url = window.URL.createObjectURL(CSVFile);
+    temp_link.href = url;
+
+    // This link should not be displayed
+    temp_link.style.display = "none";
+    document.body.appendChild(temp_link);
+
+    // Automatically click the link to
+    // trigger download
+    temp_link.click();
+    document.body.removeChild(temp_link);
+  }
+
   const handleSelectedTask = (e: any) => {
     const { checked, name } = e?.target || {};
     const id = Number(name);
@@ -75,7 +130,7 @@ export const Dashboard: React.FC = (): JSX.Element => {
       <div style={AddTaskModalButton}>
         {selectedTask?.length > 0 && (
           <Button
-            style={{ width: "40%" }}
+            style={{ width: "30%" }}
             variant="contained"
             onClick={handleDeleteTask}
           >
@@ -83,12 +138,20 @@ export const Dashboard: React.FC = (): JSX.Element => {
           </Button>
         )}
         <Button
-          style={{ width: "40%" }}
+          style={{ width: "30%" }}
           variant="contained"
           onClick={handleModalOpen}
         >
           Add Task
         </Button>
+        <Button
+          style={{ width: "30%" }}
+          variant="contained"
+          onClick={tableToCSV}
+        >
+          Download as CSV
+        </Button>
+        tableToCSV
       </div>
     </div>
   );
